0a388f436b2467fd73fcfc636f560346732c4b3b
GodBotV2 <33387081+VikramjitSinghRathee@users.noreply.github.com>
2024-02-01 17:38:59 -0500

    Commit on 2024-02-01-17:38:59

---
+++ kdp_quarto_GIT_PUSH.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change to the repository directory
+cd ~/Documents/kdp_quarto
+
+# Add all changes to git
+git add .
+
+# Commit the changes with the current date and time as the message
+git commit -m "Commit on $(date +%Y-%m-%d-%H:%M:%S)"
+
+# Push to the main branch
+git push origin main
